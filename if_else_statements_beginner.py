d63846c0f2b37daf5dcd7d675fe7f0343c26f3cd
erenkbgc <76447182+erenkbgc@users.noreply.github.com>
2021-03-10 13:16:24 +0300

    Add files via upload

---
+++ if_else_statements_beginner.py
@@ -0,0 +1,9 @@
+firstname = input("enter your first name:")
+last = input("enter your last name:")
+age =int(input("enter your age:"))
+birthday=int(input("enter your birthday:"))
+
+if age<18:
+  print('You cannot go outside because it is too dangerous')
+else:
+  print("You can go the outside")
